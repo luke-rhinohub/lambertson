91561706c6351c00364f40af9b9a7ccb311f17ab
Luke Zinn <luke@rhinohub.com>
2018-07-10 16:35:49 -0700

    Update Scripts
    Fix GTM implementation and remove unnecessary scripts

--- application/views/templates/default/en/header.php
+++ application/views/templates/default/en/header.php
@@ -33,8 +33,20 @@ header("Content-type: text/html; charset=iso-8859-1");
 		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-MNBBXBP');</script>
+		})(window,document,'script','dataLayer','GTM-KTZN4SW');</script>
 		<!-- End Google Tag Manager -->
+
+		<!-- Start Visual Website Optimizer Asynchronous Code -->
+		<script type='text/javascript'>
+		var _vwo_code=(function(){
+		var account_id=352606,
+		settings_tolerance=2000,
+		library_tolerance=2500,
+		use_existing_jquery=false,
+		/* DO NOT EDIT BELOW THIS LINE */
+		f=false,d=document;return{use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){settings_timer=setTimeout('_vwo_code.finish()',settings_tolerance);var a=d.createElement('style'),b='body{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);this.load('//dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&r='+Math.random());return settings_timer;}};}());_vwo_settings_timer=_vwo_code.init();
+		</script>
+		<!-- End Visual Website Optimizer Asynchronous Code -->
 
 		<link rel="apple-touch-icon" sizes="57x57" href="<?=base_url()?>images/favicons/apple-touch-icon-57x57.png"/>
 		<link rel="apple-touch-icon" sizes="60x60" href="<?=base_url()?>images/favicons/apple-touch-icon-60x60.png"/>
